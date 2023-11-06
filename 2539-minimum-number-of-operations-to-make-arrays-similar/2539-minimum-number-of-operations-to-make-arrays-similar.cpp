7a627e21e0769c3ad9d23dd90cd1195e8e3cb4ee
Anurag Sharma <112852778+Anurag180203@users.noreply.github.com>
2023-11-06 16:58:02 +0530

    Time: 170 ms (16.88%), Space: 87 MB (40.93%) - LeetHub

---
+++ 2539-minimum-number-of-operations-to-make-arrays-similar/2539-minimum-number-of-operations-to-make-arrays-similar.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long makeSimilar(vector<int>& nums, vector<int>& target) {
+        sort(nums.begin(),nums.end());
+        sort(target.begin(),target.end());
+        vector<int>a,b,c,d;
+        for(auto i:nums)
+        {
+            if(i&1)
+                a.push_back(i);
+            else
+                b.push_back(i);
+        }
+        for(auto i:target)
+        {
+            if(i&1)
+                c.push_back(i);
+            else
+                d.push_back(i);
+        }
+        long long ans=0;
+        for(int i=0;i<a.size();i++)
+            ans+=(abs(a[i]-c[i])/2);
+        for(int i=0;i<b.size();i++)
+            ans+=(abs(b[i]-d[i])/2);
+        return ans/2;
+    }
+};
