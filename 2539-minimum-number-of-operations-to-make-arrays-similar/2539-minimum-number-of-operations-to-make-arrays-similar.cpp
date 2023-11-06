2e05b48c3bf679c4432e2976b49ab28148d141ea
Anurag Sharma <112852778+Anurag180203@users.noreply.github.com>
2023-11-06 16:59:02 +0530

    Time: 165 ms (29.11%), Space: 86.9 MB (62.03%) - LeetHub

--- 2539-minimum-number-of-operations-to-make-arrays-similar/2539-minimum-number-of-operations-to-make-arrays-similar.cpp
+++ 2539-minimum-number-of-operations-to-make-arrays-similar/2539-minimum-number-of-operations-to-make-arrays-similar.cpp
@@ -20,9 +20,9 @@ public:
         }
         long long ans=0;
         for(int i=0;i<a.size();i++)
-            ans+=(abs(a[i]-c[i])/2);
+            ans+=(abs(a[i]-c[i]));
         for(int i=0;i<b.size();i++)
-            ans+=(abs(b[i]-d[i])/2);
-        return ans/2;
+            ans+=(abs(b[i]-d[i]));
+        return ans/4;
     }
 };
